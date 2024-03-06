2f448daa2a14a04445aad3dc0b80e69d56911b7b
Sciampa <sciampadev@proton.me>
2024-03-06 22:47:30 +0100

    init2

--- src/hooks/pool/query/usePoolList.ts
+++ src/hooks/pool/query/usePoolList.ts
@@ -19,8 +19,8 @@ export const usePoolList = () => {
 		async () => {
 			const response = await fetch(
 				import.meta.env.VITE_NEUTRONNETWORK === "neutrontestnet"
-					? "https://backend-proxy-1.onrender.com/poolList"
-					: "https://backend-proxy-1.onrender.com/poolList"
+					? "https://backend-1-h514.onrender.com/poolList"
+					: "https://backend-1-h514.onrender.com/poolList"
 			)
 			const responseJson = await response.json()
 			const pools: TPool[] = []
